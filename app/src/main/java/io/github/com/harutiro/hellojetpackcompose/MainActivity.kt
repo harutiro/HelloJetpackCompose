ebbe2f62a7e7210046edac513a07166a845a1d5d
harutiro <harutiro2027@outlook.jp>
2022-05-13 16:17:26 +0900

    add:背景を暗くできるようにした

--- app/src/main/java/io/github/com/harutiro/hellojetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/hellojetpackcompose/MainActivity.kt
@@ -1,34 +1,100 @@
 package io.github.com.harutiro.hellojetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.com.harutiro.hellojetpackcompose.ui.theme.HelloJetpackComposeTheme
+import io.github.com.harutiro.hellojetpackcompose.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContent {
-            MessageCard("Android")
+            HelloJetpackComposeTheme{
+                MessageCard(
+                    msg = Message("Android", "Jetpack Compose")
+                )
+            }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(name: String){
-    Text(text = "Hello $name")
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                //周りの
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+
+            //白背景を作る
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+        }
+    }
 }
 
-@Preview
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard(){
-    MessageCard(name = "Android")
+    HelloJetpackComposeTheme{
+        Surface() {
+            MessageCard(
+                msg = Message("Android", "Jetpack Compose")
+            )
+        }
+
+    }
 }
--- app/src/main/java/io/github/com/harutiro/hellojetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/io/github/com/harutiro/hellojetpackcompose/ui/theme/Theme.kt
@@ -1,30 +1,36 @@
 package io.github.com.harutiro.hellojetpackcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+
+    background = Color.Black,
+    surface = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
 
-    /* Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+
 )
 
 @Composable
@@ -38,10 +44,16 @@ fun HelloJetpackComposeTheme(
         LightColorPalette
     }
 
+    Surface(
+        color = MaterialTheme.colors.primarySurface,
+    ){}
+
+
     MaterialTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
+
     )
 }
