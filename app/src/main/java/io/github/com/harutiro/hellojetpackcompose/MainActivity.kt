975ed4fdcaef1280e7211c5fbec290ef34b7931e
harutiro <harutiro2027@outlook.jp>
2022-05-13 16:46:46 +0900

    add: タッチされて、広げるアニメーションを起動する。

--- app/src/main/java/io/github/com/harutiro/hellojetpackcompose/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/hellojetpackcompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,7 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -28,9 +29,11 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             HelloJetpackComposeTheme{
-                MessageCard(
-                    msg = Message("Android", "Jetpack Compose")
-                )
+//                MessageCard(
+//                    msg = Message("Android", "Jetpack Compose")
+//                )
+
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -127,21 +130,36 @@ fun MessageCard(msg: Message){
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+
+        // We toggle the isExpanded variable when we click on this Column
+        //タッチされたら、isExpanded が　Trueになる
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
+
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
 
             //白背景を作る
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2,
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+//                    広げるアニメーション
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
                 )
             }
         }
